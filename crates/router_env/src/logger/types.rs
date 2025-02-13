8d0bfc0c7f4b46a925873f9dfc2c4dc60cfff064
Mrudul Vajpayee <mrudul.vajpayee@mrudulvajpayee-XJWXCWP7HF.local>
2025-02-13 14:23:37 +0530

    handle errors

--- crates/router_env/src/logger/types.rs
+++ crates/router_env/src/logger/types.rs
@@ -543,6 +543,8 @@ pub enum Flow {
     CreateCardsInfo,
     /// Update Cards Info flow
     UpdateCardsInfo,
+    /// Cards Info migrate flow
+    CardsInfoMigrate,
 }
 
 /// Trait for providing generic behaviour to flow metric
